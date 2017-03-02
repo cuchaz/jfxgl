f454b54566bafa68f0b01c836118aae3ff2922e6
jeff <devnull@localhost>
2017-03-02 10:53:24 -0500

    simplify TexturedQuad init/rendering

--- src/com/sun/prism/es2/TexturedQuad.java
+++ src/com/sun/prism/es2/TexturedQuad.java
@@ -3,22 +3,24 @@ package com.sun.prism.es2;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
-import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
+import org.lwjgl.system.MemoryStack;
 
 public class TexturedQuad {
 	
 	public static class Shader {
 		
-		private JFXGLContext context;
-		private int id = 0;
-		private int vertexId = 0;
-		private int fragmentId = 0;
-		private int viewSizeLoc = 0;
+		private final JFXGLContext context;
+		private final int id;
+		private final int vertexId;
+		private final int fragmentId;
+		public final int posLoc;
+		public final int texCoordLoc;
+		private final int viewSizeLoc;
 		
 		public Shader(JFXGLContext context) {
 			
@@ -27,11 +29,13 @@ public class TexturedQuad {
 			// make the shader
 			vertexId = context.compileShader(Shader.class.getResource("vertex.glsl"), true);
 			fragmentId = context.compileShader(Shader.class.getResource("fragment.glsl"), false);
+			posLoc = 0;
+			texCoordLoc = 1;
 			id = context.createProgram(
 				vertexId,
 				new int[] { fragmentId },
 				new String[] { "inPos", "inTexCoord" },
-				new int[] { 0, 1 }
+				new int[] { posLoc, texCoordLoc }
 			);
 			
 			viewSizeLoc = context.getUniformLocation(id, "viewSize");
@@ -46,30 +50,17 @@ public class TexturedQuad {
 		}
 		
 		public void cleanup() {
-			if (vertexId != 0) {
-				context.deleteShader(vertexId);
-				vertexId = 0;
-			}
-			if (fragmentId != 0) {
-				context.deleteShader(fragmentId);
-				fragmentId = 0;
-			}
-			if (id != 0) {
-				context.deleteProgram(id);
-				id = 0;
-			}
+			context.deleteShader(vertexId);
+			context.deleteShader(fragmentId);
+			context.deleteProgram(id);
 		}
 	}
 	
-	private Shader shader = null;
-	private int vaoId = 0;
-	private int vboId = 0;
-	private int tboId = 0;
-	private int iboId = 0;
-	private int texId = 0;
-	private ByteBuffer indexBuf = null;
-	private FloatBuffer vertexBuf = null;
-	private FloatBuffer texCoordBuf = null;
+	private final Shader shader;
+	private final int vaoId;
+	private final int vboId;
+	private final int iboId;
+	private final int texId;
 	
 	public TexturedQuad(int x, int y, int w, int h, int texId, Shader shader) {
 		
@@ -79,48 +70,34 @@ public class TexturedQuad {
 		// make the vertex array
 		vaoId = GL30.glGenVertexArrays();
 		GL30.glBindVertexArray(vaoId);
+		
+		try (MemoryStack m = MemoryStack.stackPush()) {
 	
-		// make the indices
-		indexBuf = BufferUtils.createByteBuffer(6);
-		indexBuf.put(new byte[] {
-			0, 1, 2,
-			0, 2, 3
-		});
-		indexBuf.flip();
-		iboId = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, iboId);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, indexBuf, GL15.GL_STATIC_DRAW);
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, iboId);
-		
-		// make the vertices
-		vertexBuf = BufferUtils.createFloatBuffer(8);
-		vertexBuf.put(new float[] {
-			x + 0, y + 0,
-			x + w, y + 0,
-			x + w, y + h,
-			x + 0, y + h
-		});
-		vertexBuf.flip();
-		vboId = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertexBuf, GL15.GL_STATIC_DRAW);
-		GL20.glEnableVertexAttribArray(0);
-		GL20.glVertexAttribPointer(0, 2, GL11.GL_FLOAT, false, 0, 0);
-		
-		// make the tex coords
-		texCoordBuf = BufferUtils.createFloatBuffer(8);
-		texCoordBuf.put(new float[] {
-			0, 0,
-			1, 0,
-			1, 1,
-			0, 1
-		});
-		texCoordBuf.flip();
-		tboId = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, tboId);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, texCoordBuf, GL15.GL_STATIC_DRAW);
-		GL20.glEnableVertexAttribArray(1);
-		GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
+			// make the indices
+			ByteBuffer indexBuf = m.bytes(new byte[] {
+				0, 1, 2,
+				0, 2, 3
+			});
+			iboId = GL15.glGenBuffers();
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, iboId);
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, indexBuf, GL15.GL_STATIC_DRAW);
+			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, iboId);
+			
+			// make the vertices
+			FloatBuffer vertexBuf = m.floats(new float[] {
+				x + 0, y + 0, 0, 0,
+				x + w, y + 0, 1, 0,
+				x + w, y + h, 1, 1,
+				x + 0, y + h, 0, 1
+			});
+			vboId = GL15.glGenBuffers();
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertexBuf, GL15.GL_STATIC_DRAW);
+			GL20.glEnableVertexAttribArray(0);
+			GL20.glVertexAttribPointer(0, 2, GL11.GL_FLOAT, false, Float.BYTES*4, 0);
+			GL20.glEnableVertexAttribArray(1);
+			GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, Float.BYTES*4, Float.BYTES*2);
+		}
 		
 		// unbind things
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
@@ -144,22 +121,9 @@ public class TexturedQuad {
 	}
 	
 	public void cleanup() {
-		if (vaoId != 0) {
-			GL30.glDeleteVertexArrays(vaoId);
-			vaoId = 0;
-		}
-		if (vboId != 0) {
-			GL15.glDeleteBuffers(vboId);
-			vboId = 0;
-		}
-		if (tboId != 0) {
-			GL15.glDeleteBuffers(tboId);
-			tboId = 0;
-		}
-		if (iboId != 0) {
-			GL15.glDeleteBuffers(iboId);
-			iboId = 0;
-		}
+		GL30.glDeleteVertexArrays(vaoId);
+		GL15.glDeleteBuffers(vboId);
+		GL15.glDeleteBuffers(iboId);
 		// NOTE: don't cleanup the shader
 	}
 }
