5778ece35b2e93e042a84c0997ec5fb1b552df35
jeff <devnull@localhost>
2017-03-07 00:26:21 -0500

    add license info

--- build/def/cuchaz/jfxgl/Build.java
+++ build/def/cuchaz/jfxgl/Build.java
@@ -1,3 +1,12 @@
+/*************************************************************************
+ * Copyright (C) 2017, Jeffrey W. Martin "Cuchaz"
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2 with
+ * the classpath exception, as published by the Free Software Foundation.
+ * 
+ * See LICENSE.txt in the project root folder for the full license.
+ *************************************************************************/
 package cuchaz.jfxgl;
 
 import java.io.File;
