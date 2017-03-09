07ef29a2446b5d758882ca22a1f0391377e5cf56
jeff <devnull@localhost>
2017-03-09 12:22:35 -0500

    update build script to work on release version of jerkar

--- build/def/cuchaz/jfxgl/Build.java
+++ build/def/cuchaz/jfxgl/Build.java
@@ -10,17 +10,19 @@
 package cuchaz.jfxgl;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.jerkar.api.depmanagement.JkDependencies;
 import org.jerkar.api.depmanagement.JkFileSystemDependency;
 import org.jerkar.api.depmanagement.JkModuleDependency;
 import org.jerkar.api.depmanagement.JkModuleId;
-import org.jerkar.api.depmanagement.JkRepos;
 import org.jerkar.api.depmanagement.JkScopedDependency;
 import org.jerkar.api.depmanagement.JkVersion;
 import org.jerkar.api.file.JkFileTreeSet;
 import org.jerkar.api.file.JkPathFilter;
 import org.jerkar.api.java.JkJavaCompiler;
+import org.jerkar.api.utils.JkUtilsIO;
 import org.jerkar.tool.builtins.eclipse.JkBuildPluginEclipse;
 import org.jerkar.tool.builtins.javabuild.JkJavaBuild;
 import org.jerkar.tool.builtins.javabuild.JkJavaPacker;
@@ -71,50 +73,79 @@ public class Build extends JkJavaBuild {
 			// NOTE: ideally these would be referenced projects in Eclipse (better IDE integration that way),
 			// but jerkar wants to always compile sub-projects and I don't know how to tell it no
 			// fix incoming, see github issue: https://github.com/jerkar/jerkar/issues/61
-			.on(new File("../openjfx/modules/controls/build/classes/main")).scope(PROVIDED)
-			.on(new File("../openjfx/modules/fxml/build/classes/main")).scope(PROVIDED)
-			.on(new File("../openjfx/modules/graphics/build/classes/main")).scope(PROVIDED)
-			.on(new File("../openjfx/modules/base/build/classes/main")).scope(PROVIDED)
+			.on(new File("../openjfx/modules/controls/bin")).scope(PROVIDED)
+			.on(new File("../openjfx/modules/fxml/bin")).scope(PROVIDED)
+			.on(new File("../openjfx/modules/graphics/bin")).scope(PROVIDED)
+			.on(new File("../openjfx/modules/base/bin")).scope(PROVIDED)
 			
 			// 3rd-party libs
 			.on("org.ow2.asm:asm:5.2")
-			.on(lwjgl("3.1.1", "glfw", "jemalloc", "opengl"))
+			.on(lwjgl(this, "3.1.1", "glfw", "jemalloc", "opengl"))
 			
 			.build();
 	}
+
+	/* TEMP
+	@Override
+	public JkDependencies dependencies() {
+		return JkDependencies.builder()
+			.on("org.lwjgl:lwjgl:3.1.1")
+			.on("org.lwjgl:lwjgl:3.1.1:natives-linux")
+			.build();
+	}
+	*/
 	
-	public static JkDependencies lwjgl(String version, String ... modules) {
-		JkDependencies deps = lwjgl(version, (String)null);
+	public static JkDependencies lwjgl(JkJavaBuild build, String version, String ... modules) {
+		JkDependencies deps = lwjgl(build, version, (String)null);
 		for (String module : modules) {
-			deps = deps.and(lwjgl(version, module));
+			deps = deps.and(lwjgl(build, version, module));
 		}
 		return deps;
 	}
 	
-	public static JkDependencies lwjgl(String version, String module) {
+	public static JkDependencies lwjgl(JkJavaBuild build, String version, String module) {
 		String name = "lwjgl";
 		if (module != null) {
 			name += "-" + module;
 		}
 		String desc = "org.lwjgl:" + name + ":" + version;
-		return JkDependencies.of(JkScopedDependency.of(JkModuleDependency.of(desc)))
-			.and(allNatives(desc));
-	}
-	
-	public static JkDependencies allNatives(String desc) {
-		return JkDependencies.of(RUNTIME,
-			natives(desc, "linux"),
-			natives(desc, "windows"),
-			natives(desc, "macos")
+		JkModuleDependency dep = JkModuleDependency.of(desc);
+		return JkDependencies.of(
+			JkScopedDependency.of(dep, COMPILE),
+			JkScopedDependency.of(classifiedJar(build, dep, "natives-linux"), RUNTIME),
+			JkScopedDependency.of(classifiedJar(build, dep, "natives-windows"), RUNTIME),
+			JkScopedDependency.of(classifiedJar(build, dep, "natives-macos"), RUNTIME)
 		);
 	}
 	
-	public static JkFileSystemDependency natives(String desc, String os) {
+	public static JkFileSystemDependency classifiedJar(JkJavaBuild build, JkModuleDependency dep, String classifier) {
 		// NOTE: better support for natives in Jerkar is in-progress
 		// this is method a hacky workaround
 		// see: https://github.com/jerkar/jerkar/issues/60
-		JkModuleDependency modDep = JkModuleDependency.of(desc + ":natives-" + os);
-		return JkFileSystemDependency.of(JkRepos.mavenCentral().get(modDep));
+		
+		// just manually download the jar from Maven Central, eg at URL:
+		// http://search.maven.org/remotecontent?filepath=org/lwjgl/lwjgl/3.1.1/lwjgl-3.1.1-natives-linux.jar
+		// http://search.maven.org/remotecontent?filepath=org/lwjgl/lwjgl-opengl/3.1.1/lwjgl-opengl-3.1.1-natives-linux.jar
+		
+		String filename = String.format("%s-%s-%s.jar", dep.moduleId().name(), dep.versionRange(), classifier);
+		File file = new File(build.file("build/libs/runtime"), filename);
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+			try {
+				// NOTE: use "https" protocol here, not "http"
+				// Maven Central will redirect http->https, but Java's URLConnection will not follow cross-protocol redirects
+				URL url = new URL(String.format("https://search.maven.org/remotecontent?filepath=%s/%s/%s/%s",
+					dep.moduleId().group().replaceAll("\\.", "/"),
+					dep.moduleId().name(),
+					dep.versionRange(),
+					filename
+				));
+				JkUtilsIO.copyUrlToFile(url, file);
+			} catch (MalformedURLException ex) {
+				throw new Error(ex);
+			}
+		}
+		return JkFileSystemDependency.of(file);
 	}
 	
 	@Override
